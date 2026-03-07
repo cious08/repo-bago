17fc05bbc5e80eea30f7240bbf8032b448557be5
Precious <magcalas11082005@gmail.com>
2026-03-07 11:37:29 +0800

    update api url

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // Replace this with YOUR actual Render URL
-    baseURL: 'https://mental-health-api-2-2tt3.onrender.com/api' 
+    baseURL: 'https://mental-health-backend-ox4q.onrender.com' 
 });
 
 export default api;
